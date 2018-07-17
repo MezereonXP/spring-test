6d359af071e88720f9e5016336d182fd0add0155
Ke <mezereonxp@gmail.com>
2018-07-17 23:47:04 +0800

    [UPDATE] Change method for selecting good and teamshop by id

--- src/main/java/com/mezereon/springtest/controller/TeamShopGoodsController.java
+++ src/main/java/com/mezereon/springtest/controller/TeamShopGoodsController.java
@@ -1,10 +1,13 @@
 package com.mezereon.springtest.controller;
 
 import com.mezereon.springtest.bean.TeamShopGoods;
+import com.mezereon.springtest.dao.GoodsMapper;
 import com.mezereon.springtest.dao.TeamShopGoodsMapper;
 import com.mezereon.springtest.response.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 /**
  * Created by Administrator on 2018/07/17.
@@ -15,13 +18,20 @@ public class TeamShopGoodsController {
     @Autowired
     private TeamShopGoodsMapper teamShopGoodsMapper;
 
-    @RequestMapping(value = "/api/selectAllTeamShopGoods", method = RequestMethod.GET)
+    @Autowired
+    private GoodsMapper goodsMapper;
+
+    @RequestMapping(value = "/api/selectTeamShopGoodsById", method = RequestMethod.GET)
     @CrossOrigin
-    public Response selectAllTeamShopGoods(@RequestParam(value = "id", required = true) Integer id) {
+    public Response selectTeamShopGoodsById(@RequestParam(value = "id", required = true) Integer id) {
         Response response = new Response();
         try {
             response.setStatus(true);
-            response.setData(teamShopGoodsMapper.selectByPrimaryKey(id));
+            TeamShopGoods teamShopGoods = teamShopGoodsMapper.selectByPrimaryKey(id);
+            ArrayList<Object> list = new ArrayList<>();
+            list.add(teamShopGoods);
+            list.add(goodsMapper.selectByPrimaryKey(teamShopGoods.getTgGoodsId()));
+            response.setData(list);
             return response;
         } catch (Exception e) {
             response.setMsg(e.getMessage());
